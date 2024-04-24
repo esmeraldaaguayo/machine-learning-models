5f337ecdf08386cb1ed9a340145636092c2dfbf7
eaguayo <esmeralda.aguayo@gmail.com>
2024-04-24 14:23:31 -0700

    add mlp generator

---
+++ Pytorch/sequence_modeling/experiments/README.md
@@ -0,0 +1,12 @@
+## Data
+### UNC dataset
+kinases-ligands dataset from the university of North Carolina.
+
+### DAVIS dataset
+Kinases inhibitors - Kinases interactions (Kd) dataset.
+
+
+## References
+David H. Drewry, Carrow I. Wells, David M. Andrews, Richard Angell,Hassan Al-Ali, Alison D. Axtman, Stephen J. Capuzzi, Jonathan M. Elkins,Peter Ettmayer, Mathias Frederiksen, Opher Gileadi, Nathanael Gray, AliceHooper, Stefan Knapp, Stefan Laufer, Ulrich Luecking, Michael Michaelides,Susanne M ̈uller, Eugene Muratov, R. Aldrin Denny, Kumar S. Saikatendu,Daniel K. Treiber, William J. Zuercher, and Timothy M. Willson. Progresstowards a public chemogenomic set for protein kinases and a call for contri-butions.PLOS ONE, 12(8):1–20, 08 2017.
+
+Davis, M., Hunt, J., Herrgard, S., Ciceri, P., Wodicka, L., Pallares, G., Hocker, M., Treiber, D. and Zarrinkar, P. (2011). Comprehensive analysis of kinase inhibitor selectivity. Nature Biotechnology, 29(11), pp.1046-1051.
--- Pytorch/sequence_modeling/models/mlp.py
+++ Pytorch/sequence_modeling/models/mlp.py
@@ -10,7 +10,7 @@ class MLPRegression(nn.Module):
             hidden_dims1,
             hidden_dims2,
             hidden_dims3,
-            output_dims=1,):
+            output_dims,):
         super(MLPRegression, self).__init__()
         self.fc_linear1 = nn.Linear(input_dims, hidden_dims1)
         self.fc_linear2 = nn.Linear(hidden_dims1, hidden_dims2)
@@ -22,3 +22,47 @@ class MLPRegression(nn.Module):
         x = F.relu(self.fc_linear2(x))
         x = F.relu(self.fc_linear3(x))
         return self.fc_linear4(x)
+
+
+class MLPGenerator(nn.Module):
+    def __init__(
+            self,
+            in_features,
+            out_features,
+            hidden_units = [],
+            hidden_activation = nn.ReLU()
+    ):
+        super(MLPGenerator, self).__init__()
+
+        # model attributes
+        self.model = nn.ModuleList()
+        self.in_features = in_features
+        self.out_features = out_features
+
+        # assemble model
+        if hidden_units:
+            # establish input layer
+            self.model.append(nn.Linear(in_features, hidden_units[0]))
+            self.model.append(hidden_activation)
+
+            # set up remaining layers
+            for idx in range(len(hidden_units)-1):
+                in_size, out_size = hidden_units[idx], hidden_units[idx+1]
+                self.model.append(nn.Linear(in_size, out_size))
+                self.model.append(hidden_activation)
+        else:
+            # then model is a linear transformation
+            hidden_units = [in_features]
+
+        # output layer and final sequential
+        output_units = out_features
+        self.model.append(nn.Linear(hidden_units[-1], output_units))
+
+    def forward(self, x):
+        for f in self.model:
+            x = f(x)
+        return x
+
+
+
+
